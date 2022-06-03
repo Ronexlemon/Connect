5289867c57a9c7fe3955fe0e6e464af79a3f504a
RonexLemon <ronexondimu@gmail.com>
2022-06-03 08:26:17 +0300

    added the landpage

--- app/src/main/java/com/example/connect/LandingPageFragment.kt
+++ app/src/main/java/com/example/connect/LandingPageFragment.kt
@@ -1,59 +1,50 @@
 package com.example.connect
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.connect.composeuis.LandingPage
+import com.example.connect.databinding.FragmentLandingPageBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LandingPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LandingPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentLandingPageBinding
+    private lateinit var  handler: Handler
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing_page, container, false)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_landing_page,container,false)
+        (requireActivity() as AppCompatActivity).supportActionBar!!.hide()
+        binding.compose.setContent {
+LandingPage()
+        }
+ navi()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LandingPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LandingPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onResume() {
+        super.onResume()
+        navi()
     }
+    private fun navi() {
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            findNavController().navigate(R.id.action_landingPageFragment_to_homeFragment)
+
+        }, 2000)
+    }
+
 }
---
+++ app/src/main/java/com/example/connect/composeuis/ComposeUis.kt
@@ -0,0 +1,35 @@
+package com.example.connect.composeuis
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.connect.R
+
+@Composable
+fun LandingPage(modifier: Modifier =Modifier){
+    Column(modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally){
+
+            Text(text= stringResource(R.string.Appland), style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.ExtraBold))
+            Image(painter= painterResource(R.drawable.connect),contentDescription = null,modifier=Modifier.size(width = 500.dp, height = 500.dp))
+            Text(text= stringResource(R.string.landdown), style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.ExtraBold))
+
+
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    LandingPage()
+}
